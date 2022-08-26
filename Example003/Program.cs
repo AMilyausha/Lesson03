24e3f7da201d506e37441fb18713424e11540998
Amirova Milyausha <mila17.86@mail.ru>
2022-08-26 23:13:13 +0300

    Третье задание

---
+++ Example003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+int num=1;
+for (int i=0;i<n;i++)
+   {
+    a[i]= num*num*num;
+   num++; 
+    Console.WriteLine(a[i]);
+   } 
+  
