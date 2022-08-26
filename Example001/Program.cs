8220a7e33164a956135747c65bd86ba8f5552ae5
Amirova Milyausha <mila17.86@mail.ru>
2022-08-26 22:21:18 +0300

    Первое задание

---
+++ Example001/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int n =int.Parse(Console.ReadLine());
+int a1 = n/10000; // первая цифра
+Console.WriteLine(a1);
+int a2 = (n%100) %10; // пятая цифра
+Console.WriteLine(a2);
+int b1 = (n/1000)%10; // вторая цифра
+Console.WriteLine(b1);
+int b2 = (n%100) /10; // четвертая цифра
+Console.WriteLine(b2);
+if ((a1==a2) && (b1==b2))
+{
+   Console.WriteLine("Число является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом.");
+}
